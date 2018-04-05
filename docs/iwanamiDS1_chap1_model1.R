e639a771e8a1658d16c1abead517e6bb08ad1796
iypod <wrof80@gmail.com>
2018-04-06 08:12:22 +0900

    Stanコードの分離

--- docs/iwanamiDS1_chap1_model1.R
+++ docs/iwanamiDS1_chap1_model1.R
@@ -1,6 +1,6 @@
-# 参照 ----------------------------------------------------------------------
-https://rpubs.com/uri-sy/iwanami_ds1
-http://statmodeling.hatenablog.com/entry/iwanami-datascience-vol1
+# 参考 ----------------------------------------------------------------------
+# https://rpubs.com/uri-sy/iwanami_ds1
+# http://statmodeling.hatenablog.com/entry/iwanami-datascience-vol1
 
 # データ読込 -------------------------------------------------------------------
 # download.file(url = "http://hosho.ees.hokudai.ac.jp/~kubo/stat/iwnmDS1/data/data.RData",
@@ -15,12 +15,21 @@ library(tidyverse); library(rstan)
 options(mc.cores = parallel::detectCores())
 rstan_options(auto_write = TRUE)
 
-# Stanコード -----------------------------------------------------------------
-
-model1 <- 
-  
-
-
 # Rコード --------------------------------------------------------------------
 
 
+data <- list(
+  N=nrow(d),
+  Mean_Y=d$mean.Y,
+  X=d$X,
+  Age=d$Age
+)
+
+stanmodel1 <- stan_model(file="docs/iwanamiDS1_chap1_model1.stan")
+
+fit1 <-
+  sampling(stanmodel1,
+       data=d,
+       iter=1000, warmup=200, seed=1)
+
+save.image()
